0ece2fa056f779c67a39bd74ac4c8168dac0bcf7
UeriAugustineBolo <ueriaugustine.bolo.cics@ust.edu.ph>
2025-12-05 12:51:21 +0800

    Update admin_transact.php

--- admin_transact.php
+++ admin_transact.php
@@ -2,7 +2,7 @@
 session_start();
 
 $user = 'root';
-$password = 'D1dhen1102';
+$password = '123456';
 $database = 'InternetCafe';
 $servername = 'localhost:3310';
 
@@ -180,5 +180,6 @@ $resultTransact = $mysqli->query($sql);
     </form>
 
 </body>
+
 
 </html>
