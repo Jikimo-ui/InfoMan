dff5c54d90b3e85254d927f1d10e7a24ec732402
UeriAugustineBolo <ueriaugustine.bolo.cics@ust.edu.ph>
2025-12-10 15:44:58 +0800

    Change database password for login script

--- process_login_end_user.php
+++ process_login_end_user.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $user = 'root';
-$password = 'D1dhen1102';
+$password = '123456';
 $database = 'InternetCafe';
 $servername = 'localhost:3310';
 
@@ -32,3 +32,4 @@ $_SESSION['error'] = "Wrong username or password!";
 $mysqli->close();
 header("Location: login.php");
 exit();
+
